47cf92b2e8da7485504a6eca7a9261b754f28667
Encode42 <me@encode42.dev>
2023-08-15 16:47:42 -0400

    Use TAW for CriteriaInit
    Allows for compatibility with Quilt.
    
    Entirely based on the work of https://github.com/Globox1997/LevelZ/pull/286

--- src/main/java/net/jobsaddon/init/CriteriaInit.java
+++ src/main/java/net/jobsaddon/init/CriteriaInit.java
@@ -1,14 +1,13 @@
 package net.jobsaddon.init;
 
-import net.fabricmc.fabric.mixin.object.builder.CriteriaAccessor;
 import net.jobsaddon.criteria.JobUpCriterion;
-import net.levelz.mixin.misc.ScoreboardCriterionAccessor;
+import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.scoreboard.ScoreboardCriterion;
 
 public class CriteriaInit {
 
-    public static final JobUpCriterion JOB_UP = CriteriaAccessor.callRegister(new JobUpCriterion());
-    public static final ScoreboardCriterion JOBS = ScoreboardCriterionAccessor.callCreate("jobsaddon");
+    public static final JobUpCriterion JOB_UP = Criteria.register(new JobUpCriterion());
+    public static final ScoreboardCriterion JOBS = ScoreboardCriterion.create("jobsaddon");
 
     public static void init() {
     }
